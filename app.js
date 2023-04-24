ca77b019800ae58123f440bc1cf851ca67499f57
mandriva <rhyaawp@gmail.com>
2023-04-24 16:41:26 +0400

    v-0.8
    added click counter

--- app.js
+++ app.js
@@ -3,17 +3,32 @@ const header = document.querySelector('.header')
 const footer = document.querySelector('.footer')
 const btn = document.querySelector('.btn')
 const footerText = document.querySelector('.footer-text')
+//counter display text
+const cnp = document.querySelector('.click-counter-text')
+//counter display number
+const counterDisplayArea = document.querySelector('.counter-display')
 //define the colors array
 const colors = ["#da9f64", "#f67dd1", "red", "blue", "white", "cyan", "orange", "purple", "yellow", "green", "brown", "gray", "#e0cb9a", "#348704", "#bfff4f", "#c6ecc3", "#a081e2","#c39feb",  "#022504", "#41f635", "#4c856a", "#cedd43", "#041d30", "#9c499f"];
 //colors for footer
 const colorsff = ["#da9f64", "#f67dd1", "red", "blue", "white", "cyan", "orange", "purple", "yellow", "green", "brown", "gray", "#e0cb9a", "#348704", "#bfff4f", "#c6ecc3", "#a081e2","#c39feb",  "#022504", "#41f635", "#4c856a", "#cedd43", "#041d30", "#9c499f"];
 //create function
 
+let ffcounter = document.querySelector('.ffcounter'),
+  count = 0;
+ffcounter.onclick = function() {
+  count += 1;
+  counterDisplayArea.innerHTML = count;
+};
+
+//dsfnds
+
 btn.addEventListener('click', function(){
     const randomIndex = Math.floor(Math.random() * colors.length);
     const randomColor = colors[randomIndex];
     header.style.backgroundColor = randomColor;
-    footerText.classList.toggle('d-none')
+    footerText.classList.remove('d-none')
+    cnp.classList.remove('d-none')
+
 })
 
 btn.addEventListener('click', function(){
@@ -36,3 +51,4 @@ const changeColorff = function (){
 
 changeColor()
 changeColorff()
+

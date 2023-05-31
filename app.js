a42f0ad8c654430e071cda3054eb89642ce088dc
mandriva19 <97590591+mandriva19@users.noreply.github.com>
2023-05-31 23:17:25 +0400

    0.9945 - beta

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const header = document.querySelector('.header')
 const footer = document.querySelector('.footer')
 const btn = document.querySelector('.btn')
 const footerText = document.querySelector('.footer-text')
+const winningOverlay = document.querySelector('.winning-overlay');
 
 //counter display text
 const counterDisplayText = document.querySelector('.click-counter-text')
@@ -11,10 +12,10 @@ const counterDisplayText = document.querySelector('.click-counter-text')
 const counterDisplayArea = document.querySelector('.counter-display')
 
 
-//define the colors 
-const colors = ['hsl(234, 92%, 60%)', 'hsl(45, 87%, 50%)', 'hsl(189, 67%, 55%)', 'hsl(321, 62%, 60%)', 'hsl(156, 100%, 45%)', 'hsl(278, 79%, 60%)', 'hsl(82, 100%, 45%)', 'hsl(11, 88%, 55%)', 'hsl(205, 70%, 65%)', 'hsl(349, 85%, 60%)', 'hsl(60, 100%, 50%)', 'hsl(171, 60%, 50%)', 'hsl(296, 79%, 50%)', 'hsl(25, 75%, 55%)', 'hsl(205, 85%, 60%)', 'hsl(345, 65%, 55%)', 'hsl(114, 100%, 40%)', 'hsl(225, 60%, 45%)', 'hsl(165, 70%, 50%)', 'hsl(305, 60%, 50%)', 'hsl(40, 100%, 50%)', 'hsl(195, 85%, 60%)', 'hsl(330, 65%, 55%)', 'hsl(124, 100%, 40%)', 'hsl(235, 60%, 45%)', 'hsl(175, 70%, 50%)', 'hsl(315, 60%, 50%)', 'hsl(80, 100%, 50%)', 'hsl(215, 85%, 60%)', 'hsl(355, 65%, 55%)', 'hsl(134, 100%, 40%)', 'hsl(245, 60%, 45%)', 'hsl(185, 70%, 50%)', 'hsl(325, 60%, 50%)', 'hsl(100, 100%, 50%)', 'hsl(225, 85%, 60%)', 'hsl(5, 65%, 55%)', 'hsl(144, 100%, 40%)', 'hsl(255, 60%, 45%)', 'hsl(195, 70%, 50%)', 'hsl(335, 60%, 50%)', 'hsl(120, 100%, 50%)'];
+//colors for header array
+const colors = ['hsl(234, 92%, 60%)', 'hsl(45, 87%, 50%)', 'hsl(189, 67%, 55%)', 'hsl(321, 62%, 60%)', 'hsl(156, 100%, 45%)', 'hsl(278, 79%, 60%)', 'hsl(82, 100%, 45%)', 'hsl(11, 88%, 55%)', 'hsl(205, 70%, 65%)', 'hsl(349, 85%, 60%)', 'hsl(60, 100%, 50%)', 'hsl(171, 60%, 50%)', 'hsl(296, 79%, 50%)', 'hsl(25, 75%, 55%)', 'hsl(205, 85%, 60%)', 'hsl(345, 65%, 55%)', 'hsl(114, 100%, 40%)', 'hsl(225, 60%, 45%)', 'hsl(165, 70%, 50%)', 'hsl(305, 60%, 50%)', 'hsl(40, 100%, 50%)', 'hsl(195, 85%, 60%)', 'hsl(330, 65%, 55%)', 'hsl(124, 100%, 40%)', 'hsl(235, 60%, 45%)', 'hsl(175, 70%, 50%)', 'hsl(315, 60%, 50%)', 'hsl(80, 100%, 50%)', 'hsl(215, 85%, 60%)', 'hsl(355, 65%, 55%)', 'hsl(134, 100%, 40%)', 'hsl(245, 60%, 45%)', 'hsl(185, 70%, 50%)', 'hsl(325, 60%, 50%)', 'hsl(100, 100%, 50%)', 'hsl(225, 85%, 60%)', 'hsl(5, 65%, 55%)', 'hsl(144, 100%, 40%)', 'hsl(255, 60%, 45%)', 'hsl(195, 70%, 50%)', 'hsl(335, 60%, 50%)', 'hsl(120, 100%, 50%)']; 
 
-//colors for footer
+//colors for footer array
 const colorsff = ['hsl(234, 92%, 60%)', 'hsl(45, 87%, 50%)', 'hsl(189, 67%, 55%)', 'hsl(321, 62%, 60%)', 'hsl(156, 100%, 45%)', 'hsl(278, 79%, 60%)', 'hsl(82, 100%, 45%)', 'hsl(11, 88%, 55%)', 'hsl(205, 70%, 65%)', 'hsl(349, 85%, 60%)', 'hsl(60, 100%, 50%)', 'hsl(171, 60%, 50%)', 'hsl(296, 79%, 50%)', 'hsl(25, 75%, 55%)', 'hsl(205, 85%, 60%)', 'hsl(345, 65%, 55%)', 'hsl(114, 100%, 40%)', 'hsl(225, 60%, 45%)', 'hsl(165, 70%, 50%)', 'hsl(305, 60%, 50%)', 'hsl(40, 100%, 50%)', 'hsl(195, 85%, 60%)', 'hsl(330, 65%, 55%)', 'hsl(124, 100%, 40%)', 'hsl(235, 60%, 45%)', 'hsl(175, 70%, 50%)', 'hsl(315, 60%, 50%)', 'hsl(80, 100%, 50%)', 'hsl(215, 85%, 60%)', 'hsl(355, 65%, 55%)', 'hsl(134, 100%, 40%)', 'hsl(245, 60%, 45%)', 'hsl(185, 70%, 50%)', 'hsl(325, 60%, 50%)', 'hsl(100, 100%, 50%)', 'hsl(225, 85%, 60%)', 'hsl(5, 65%, 55%)', 'hsl(144, 100%, 40%)', 'hsl(255, 60%, 45%)', 'hsl(195, 70%, 50%)', 'hsl(335, 60%, 50%)', 'hsl(120, 100%, 50%)'];
 
 
@@ -23,53 +24,65 @@ const colorsff = ['hsl(234, 92%, 60%)', 'hsl(45, 87%, 50%)', 'hsl(189, 67%, 55%)
 =======================================================
 */
 
-//click counter
+//click counter function for counting click to display 
 let ffcounter = document.querySelector('.ffcounter')
   count = 0;
-ffcounter.onclick = function() {
+  ffcounter.onclick = function() {
   count += 1;
   counterDisplayArea.innerHTML = count;
 };
 
 //play audio on button 
-const audioClick = document.querySelector('.clickAudio')
+    const audioClick = document.querySelector('.clickAudio')
+    function clickSound(){
+    audioClick.play();
+}
 
-function clickSound(){
-  audioClick.play();
+//change colors function for header
+function changeColor() {
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  const randomColor = colorsff[randomIndex];
+  header.style.backgroundColor = randomColor;
+}
+
+//change colors function for footer
+function changeColorff() {
+  const randomIndexff = Math.floor(Math.random() * colorsff.length);
+  const randomColorff = colorsff[randomIndexff];
+  footer.style.backgroundColor = randomColorff;
+}
+
+//function for comparing if colors are matching
+
+function checkColorMatch() {
+  const headerColor = header.style.backgroundColor;
+  const footerColor = footer.style.backgroundColor;
+
+  if (headerColor === footerColor) {
+    // Colors match, run your function here
+    console.log("You win the game!");
+    //show winning overlay
+    winningOverlay.classList.add('winning-show');
+    //play winning audio
+    const winAudio = document.querySelector('.win-audio');
+    winAudio.play();
+  } else {
+    console.log("Click more...");
+  }
 }
 
 
-//function for random color generator
+//event listener for btn
 btn.addEventListener('click', function(){
-    const randomIndex = Math.floor(Math.random() * colors.length);
-    const randomColor = colors[randomIndex];
-    header.style.backgroundColor = randomColor;
+    //show footer text
     footerText.classList.remove('d-none')
+    //show click counter
     counterDisplayText.classList.remove('d-none')
+    //add functions inside click event
+    changeColor()
+    changeColorff()
     clickSound()
-
+    checkColorMatch()
 })
 
-btn.addEventListener('click', function(){
-    const randomIndexff = Math.floor(Math.random() * colors.length);
-    const randomColorff = colors[randomIndexff];
-    footer.style.backgroundColor = randomColorff;
-})
-
-const changeColor = function (){
-    const randomIndex = Math.floor(Math.random() * colors.length);
-    const randomColor = colors[randomIndex];
-    header.style.backgroundColor = randomColor;
-}
-
-const changeColorff = function (){
-    const randomIndex = Math.floor(Math.random() * colors.length);
-    const randomColor = colors[randomIndex];
-    footer.style.backgroundColor = randomColor;
-}
-
-// changeColor()
-// changeColorff()
-
-// run this if the colors are match
 
